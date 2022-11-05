485af05b1d929eaf7ac61cac6e72fcba451dd17e
zoey-yjj <zoeyyyjj@gmail.com>
2022-11-05 16:25:54 +0800

    define and use fetch_files_with_link to fetch images

--- filter_words.py
+++ filter_words.py
@@ -77,3 +77,40 @@ for query in QURIES:
         category_path = f'{save_path}/{query}_urls.csv'
         print(f"Writing {query} urls to {category_path}")
         urls.to_csv(category_path)
+
+"""
+1.3 Download files using acquired links
+
+Use collected image urls to download the image files.
+"""
+
+def fetch_files_with_link(url_path):
+    with open(url_path, newline="") as csvfile:
+        urls = pd.read_csv(url_path, delimiter=',')
+        urls = urls.iloc[:, 1].to_dict().values()
+        
+    SAVE_PATH = os.path.join(url_path.replace('_urls.csv', ''))
+    if not os.path.isdir(SAVE_PATH):
+        os.mkdir(SAVE_PATH) #define image storage path
+
+    for idx, url in tqdm(enumerate(urls), total=len(urls)):
+        # print("Starting download {} of ".format(url[0] + 1), len(urls))
+        try:
+            resp = requests.get(url, stream=True)   #request file using url
+            path_to_write = os.path.join(SAVE_PATH, url.split("/")[-1])
+            outfile = open(path_to_write, 'wb')
+            outfile.write(resp.content) #save file content
+            outfile.close()
+            #print("Done downloading {} of {}".format(idx + 1, len(urls)))
+        except:
+            print("Failed to download url number {}".format(idx)) 
+    print(f"Done with {url_path} download, images are saved in {SAVE_PATH}")
+
+print("Start downloading images...")
+
+
+CATEGORIES = newdata    #specify search query
+save_path = './Flickr_scrape/'
+for category in CATEGORIES:
+    url_path = f'{save_path}/{category}_urls.csv'
+    fetch_files_with_link(url_path)
